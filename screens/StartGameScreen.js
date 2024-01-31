118e6e506c96c3d0eccc28e43914da7f45b79ac9
chosohi <chosohi@naver.com>
2024-01-31 11:57:10 +0900

    section4: 경고창 표시

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,33 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const handleNumberInput = (enteredText) => {
+    setEnteredNumber(enteredText);
+  };
+
+  const handleResetInput = () => {
+    setEnteredNumber("");
+  };
+
+  const handleConfirmInput = () => {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      // show alert...
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: handleResetInput }]
+      );
+      return;
+    }
+  };
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -13,13 +38,15 @@ const StartGameScreen = () => {
         autoCapitalize="none"
         // 자동 수정 끄기
         autoCorrect={false}
+        onChangeText={handleNumberInput}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={handleResetInput}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={handleConfirmInput}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
